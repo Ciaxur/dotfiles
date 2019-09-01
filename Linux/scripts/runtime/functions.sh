01df3093ddc3ea9f1402cc1e1f5c8a429fc118c4
Omar Omar <omar41097@gmail.com>
2019-09-01 12:12:54 -0400

    Improved Redshift Nightmode Function

--- Linux/scripts/runtime/functions.sh
+++ Linux/scripts/runtime/functions.sh
@@ -4,15 +4,26 @@
 # Enables/Disables Nightmode
 # Parameters are on/off
 nightmode() {
+
     if [ "$1" = "-h" ]; then
         echo "Parameters: [on/off] to trigger nightmode"
     
     elif [ "$1" = "off" ]; then
-        redshift -x
-    
+        redshift -x        &>/dev/null
+        echo "Turning off Nightmode"
+
+    # Actual Value to set Redshift (Checks if Integer)
+    elif [ $1 -eq $1 2>/dev/null ] && [ ! -z "$1" ]; then
+        redshift -x        &>/dev/null
+        redshift -O $1     &>/dev/null
+
+        echo "Setting Nightmode to $1"
     else
-        redshift -x
-        redshift -O 3000
+        redshift -x        &>/dev/null
+        redshift -O 3000   &>/dev/null
+        
+        echo "Setting Nightmode to '3000'"
     fi
 
 }
+
