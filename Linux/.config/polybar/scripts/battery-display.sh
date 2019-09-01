24400161f878aaa8c84991585148ddcb1f45bbf2
Omar Omar <omar41097@gmail.com>
2019-09-01 15:27:16 -0400

    Added Color Indicators based on Battery States

--- Linux/.config/polybar/scripts/battery-display.sh
+++ Linux/.config/polybar/scripts/battery-display.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 
-# Obtain Percentage
+# Obtain Battery Data
 bat=`acpi -b | awk '{print $4}' | grep -o '[0-9]\+'`
+chargeState="$(acpi -b | awk '{print $3}' | sed 's/,//g')"
 
-echo " 🔋%{F#999}BAT %{F#FFF}$bat% "
+
+# Change Colors based on States
+# BAT = red ; $bat = white
+if [ $bat -lt 11 ] && [ $chargeState = "Discharging" ]; then
+    echo " 🔋%{F#FF4400}BAT %{F#FFF}$bat% "
+
+# BAT = orange ; $bat = white
+elif [ $bat -lt 21 ] && [ $chargeState = "Discharging" ]; then
+    echo " 🔋%{F#FFB300}BAT %{F#FFF}$bat% "
+
+# BAT = green ; $bat = white
+else
+    echo " 🔋%{F#37FF00}BAT %{F#FFF}$bat% "
+fi
+
+
+#echo " 🔋%{F#999}BAT %{F#FFF}$bat% "
