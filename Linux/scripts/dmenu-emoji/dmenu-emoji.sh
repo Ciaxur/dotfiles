c279625e7a9612ff2593e88f35f5b82a7198139e
Omar Omar <omar41097@gmail.com>
2019-09-06 18:02:45 -0400

    Added History

--- Linux/scripts/dmenu-emoji/dmenu-emoji.sh
+++ Linux/scripts/dmenu-emoji/dmenu-emoji.sh
@@ -9,14 +9,13 @@ chosen=$(grep -v "#" ~/scripts/dmenu-emoji/emoji-list | rofi -dmenu \
                  -location 2 -width 20 \
 		 -lines 20 -line-margin 0 -line-padding 1 \
 		 -separator-style none -font "Monospace 8" -columns 1 -bw 0 \
-		 -disable-history \
 		 -hide-scrollbar \
 		 -color-window "#002334, #002334, #002334" \
 		 -color-normal "#00344E, #7F8C8D, #003140, #195A78, #FFFFFF" \
 		 -color-active "#222222, #b1b4b3, #222222, #007763, #b1b4b3" \
 		 -color-urgent "#222222, #b1b4b3, #222222, #77003d, #b1b4b3" \
 		 -kb-row-select "Tab" -kb-row-tab "" \
-                 -i)
+         -i)
 
 [ "$chosen" != "" ] || exit
 
