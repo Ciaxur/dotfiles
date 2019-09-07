6d2bd821322b223cd63c2b5517e6974b045bc486
Omar Omar <omar41097@gmail.com>
2019-09-07 12:57:24 -0400

    Updated Spotify dbus Script

--- Linux/scripts/spotify/getCurrentPlayingSong.sh
+++ Linux/scripts/spotify/getCurrentPlayingSong.sh
@@ -8,8 +8,16 @@ DEBUG=0
 ###################################
 # Obtain data and Store into Temp #
 ###################################
-TMP_FILE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
-qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Metadata > /tmp/$TMP_FILE
+TMP_FILE="Spotify.cLB6HKS25q8F"
+qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Metadata > /tmp/$TMP_FILE 2>&1
+
+##############################
+# Make sure music is playing #
+##############################
+if [ $(cat /tmp/$TMP_FILE | wc -l) -lt 9 ];then
+    echo ""
+    return 0
+fi
 
 
 ###################################
@@ -19,7 +27,6 @@ LENGTH=$(cat /tmp/$TMP_FILE | grep length | cut -d ' ' -f2-)
 ALBUM=$(cat /tmp/$TMP_FILE | grep -iw "album" | cut -d ' ' -f2-)
 ARTIST=$(cat /tmp/$TMP_FILE | grep -iw "artist" | cut -d ' ' -f2-)
 SONG=$(cat /tmp/$TMP_FILE | grep -iw "title" | cut -d ' ' -f2-)
-
 
 #######################################
 # Convert Microseconds to Human Time  #
