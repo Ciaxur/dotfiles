1bee4c64a85d8f289ebf7566f451003bcb644d03
Ciaxur <omar41097@gmail.com>
2019-11-13 16:43:48 -0500

    Added Downloader-CLI by deepjyoti30

--- Linux/scripts/runtime/aliases.sh
+++ Linux/scripts/runtime/aliases.sh
@@ -6,8 +6,12 @@ alias open="nautilus"           # GUI File Manager
 alias kp="keepassxc.cli"        # Keepass CLI
 alias py="python3.7"            # Latest Python Alias
 
+# Alias for Downloader-CLI by "deepjyoti30"
+alias dw="py ~/documents/git-clones/downloader-cli/download.py"
+
 # Alias to Function lfcd, function.sh has to be
 #  sourced before this :)
 alias fm='lfcd'
 
-
+# Alias for Copying over Text by piping
+alias copy="tr -d '\n' | xclip -selection clipboard"
