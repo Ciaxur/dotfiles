72f025f5a9e615a05ec468948422d641405fa804
Omar Omar <omar41097@gmail.com>
2019-12-31 16:16:53 -0500

    Configuration Script Added

--- Linux/scripts/Brightness/BrightnessServiceChange/addConfig.sh
+++ Linux/scripts/Brightness/BrightnessServiceChange/addConfig.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+
+# VARIABLES
 config_file=/tmp/tmp123.brightness.conf
 val=$1
 
-if [ "$val" == "" ]; then
+# MAKE SURE FILE DOESN'T EXIST
+if [ -f $config_file ]; then
+    exit 1
+
+elif [ "$val" == "" ]; then
     val=0
 fi
 
+# ADD FILE
 printf "[Brightness]\nval=$val\n" > $config_file
