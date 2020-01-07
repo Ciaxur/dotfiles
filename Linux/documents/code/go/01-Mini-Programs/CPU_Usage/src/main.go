1b00493e6c7205cc51dc88e021b2733c46bfb2f9
Omar Omar <omar41097@gmail.com>
2020-01-07 07:06:21 -0500

    Removed Comments

--- Linux/documents/code/go/01-Mini-Programs/CPU_Usage/src/main.go
+++ Linux/documents/code/go/01-Mini-Programs/CPU_Usage/src/main.go
@@ -77,22 +77,3 @@ func main() {
 	}
 }
 
-/*
-Suppose at 14:00:00 you have
-
-cpu 4698 591 262 8953 916 449 531
-total_jiffies_1 = (sum of all values) = 16400
-work_jiffies_1 = (sum of user,nice,system = the first 3 values) = 5551
-
-
-and at 14:00:05 you have
-cpu 4739 591 289 9961 936 449 541
-total_jiffies_2 = 17506
-work_jiffies_2 = 5619
-
-
-So the %cpu usage over this period is:
-work_over_period = work_jiffies_2 - work_jiffies_1 = 68
-total_over_period = total_jiffies_2 - total_jiffies_1 = 1106
-%cpu = work_over_period / total_over_period * 100 = 6.1%
-*/
