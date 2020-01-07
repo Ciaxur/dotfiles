6d70cb16123cf95197d4541f7feff940a2c2e612
Omar Omar <omar41097@gmail.com>
2020-01-07 07:22:36 -0500

    Little Touch of Awesome Exit

--- Linux/scripts/compressPDF.sh
+++ Linux/scripts/compressPDF.sh
@@ -10,9 +10,12 @@ print_help() {
 # Verify Input :)
 if [ "$1" = "-h" ] || [ "$1" = "" ] || [ "$2" = "" ]; then
     print_help
+    exit 1
 fi
 
 
 # Preceed with Compression
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook \
        -dNOPAUSE -dBATCH  -dQUIET -sOutputFile=$2 $1
+
+exit 0
