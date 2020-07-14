be8a17791944f110a139d48c6bd24f6592133b8c
Omar Omar <oomar@umich.edu>
2020-07-14 01:41:04 -0400

    Alacritty Scrolling & Keybinds + Emoji Support

--- Linux/scripts/runtime/env.sh
+++ Linux/scripts/runtime/env.sh
@@ -1,5 +1,10 @@
 # Environment Variables
 export EDITOR=vim
-export GOPATH=~/documents/go
+export GOPATH=~/Documents/go
 export TERM=rxvt-256color
+export LESS='--mouse'
 #export TERM=st-256color
+
+# Deno
+export DENO_INSTALL="/home/omar/Documents/deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
