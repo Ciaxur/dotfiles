8b8f42afd552b998745c3258d7ea2da04d7831a4
Omar Omar <oomar@umich.edu>
2020-09-17 22:47:58 -0400

    ✨ Dotfiles Script custom Mapping
    - Cleaned up the dotfiles scripts file
    - Can have custom mapping for Source to Target for Files

--- Linux/scripts/dotfiles-update/script.sh
+++ Linux/scripts/dotfiles-update/script.sh
@@ -9,29 +9,39 @@
 
 # PATHS TO CHECK (MUST BE ABSOUTE, for guarenteed success!)
 source_path="/home/omar"
-target_path="/home/omar/documents/git-clones/dotfiles/Linux"
+target_path="/home/omar/Documents/git-clones/dotfiles/Linux"
 filesUsed_file="filesUsed.txt"
 
 # MOVE ALL SPECIFIED FILES/DIRS FROM TEXTFILE
 if [ -f $filesUsed_file ]; then
-    echo "Comparing..."
-    while read path; do
-        source="$source_path/$path"
-        target="$target_path/$path"
+  echo "Comparing..."
+  while read path; do
+    # Split on Delimeter for Customer Source->Target Path
+    IFS=":" read -a pathArr <<< $path
 
-        
-        printf "Source [$source] \t Target [$target]\n"
-        
-        # Make sure Necessary Directories Created
-        prevPath=$(dirname $target)
-        if [ ! "$prevPath" = "$target_path" ]; then
-            mkdir -p $prevPath
-            cp -ru $source $prevPath
-        else
-            cp -ru $source $target
-        fi
-    done < filesUsed.txt
+    # Specify Correct Paths
+    source="$source_path/$pathArr"          # Pick first Path from PathArr
+
+    # Check for Custom Path
+    if [ ${#pathArr[@]} = 2 ]; then
+      target="$target_path/${pathArr[1]}"
+    else
+      target="$target_path/$pathArr"
+    fi
+
+    # Verbose Output
+    printf "Source [$source] \t Target [$target]\n"
+    
+    # Make sure Necessary Directories Created
+    prevPath=$(dirname $target)
+    if [ ! "$prevPath" = "$target_path" ]; then
+      mkdir -p $prevPath
+      cp -ru $source $prevPath
+    else
+      cp -ru $source $target
+    fi
+  done < filesUsed.txt
 
 else
-    echo "Please create a 'filesUsed.txt' to use."
+  echo "Please create a 'filesUsed.txt' to use."
 fi
