f7231b2a0b25fb3d24ca3e76523207970d16b57c
Omar Omar <oomar@umich.edu>
2021-09-07 19:47:31 -0400

    Update rofi menu scripts to use v1.7.0

--- Linux/scripts/dmenu-emoji/dmenu-emoji.sh
+++ Linux/scripts/dmenu-emoji/dmenu-emoji.sh
@@ -9,16 +9,13 @@ CURR_DIR=`dirname $0`
 echo -n | xclip -sel clipboard || exit
 
 chosen=$(grep -v "#" ~/scripts/dmenu-emoji/emoji-list | rofi -dmenu -p "Emoji" \
-                 -location 2 -width 20 \
-		 -lines 20 -line-margin 0 -line-padding 1 \
-		 -separator-style none -font "JoyPixels 8" -columns 1 -bw 0 \
-		 -hide-scrollbar \
-		 -color-window "#002334, #002334, #002334" \
-		 -color-normal "#00344E, #7F8C8D, #003140, #195A78, #FFFFFF" \
-		 -color-active "#222222, #b1b4b3, #222222, #007763, #b1b4b3" \
-		 -color-urgent "#222222, #b1b4b3, #222222, #77003d, #b1b4b3" \
-		 -kb-row-select "Tab" -kb-row-tab "" \
-         -i)
+	-theme "$CURR_DIR/Theme.rasi" \
+	-location 2 -width 20 \
+	-lines 20 -line-margin 0 -line-padding 1 \
+	-separator-style none -font "JoyPixels 8" -columns 1 -bw 0 \
+	-hide-scrollbar \
+	-kb-row-select "Tab" -kb-row-tab "" \
+	-i)
 
 [ "$chosen" != "" ] || exit
 
@@ -26,9 +23,3 @@ c=$(echo "$chosen" | sed "s/ .*//")
 echo "$c" | tr -d '\n' | xclip -selection clipboard
 notify-send -a "Emoji Menu" "'$c' copied to clipboard." -i "$CURR_DIR/pericles.png" &
 
-#s=$(echo "$chosen" | sed "s/.*; //" | awk '{print $1}')
-#echo "$s" | tr -d '\n' | xclip
-#notify-send "'$s' copied to primary." &
-
-# dmenu_run -nf '#C8C8C8' -nb '#1D1D1D' 
-#  -sb '#A569BD' -sf '#EEEEEE' -fn 'monospace-10' -p 'Apps:'
