e0a1e98d93c0d8e8e6f0c02be1c2f88a85fd47ca
Omar Omar <oomar@umich.edu>
2023-09-03 20:22:47 -0700

    add worldtime script

---
+++ Linux/scripts/worldtime.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Simple script which lists tracked times across the world.
+LEB_TIME=$(TZ=EET date)
+MI_TIME=$(TZ=":US/Eastern" date)
+LOCAL_TIME=$(date)
+
+echo -e "\033[1mLocal Time:   \033[0m $LOCAL_TIME"
+echo -e "\033[1mLebanon Time: \033[0m $LEB_TIME"
+echo -e "\033[1mMichigan Time:\033[0m $MI_TIME"
