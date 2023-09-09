7d572569621b94e968a379908ffd6c33f55666f4
Omar Omar <oomar@umich.edu>
2023-09-09 14:20:52 -0700

    add bat config to env + qt stye override

--- Linux/scripts/runtime/env.sh
+++ Linux/scripts/runtime/env.sh
@@ -7,6 +7,7 @@ export LESS='--mouse -RX'
 export ANDROID_SDK=~/Documents/Android/Sdk
 export SSH_AUTH_SOCK="/run/user/1000/ssh-agent.socket"
 export SUDO_ASKPASS=/usr/lib/ssh/ssh-askpass
+export BAT_CONFIG_PATH=~/.config/bat/bat.conf
 
 
 # Ruby Path
@@ -15,3 +16,7 @@ export RUBY_PATH="/home/omar/.gem/ruby/2.7.0"
 # Export Path
 export PATH="$RUBY_PATH/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
+
+# GUI Themes
+## https://archlinux.org/packages/extra/x86_64/kvantum
+export QT_STYLE_OVERRIDE=Oxygen
