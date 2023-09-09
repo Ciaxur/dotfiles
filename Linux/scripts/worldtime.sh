0b9eaf1b5c82d1cc4e7897e915e0f92634d41db3
Omar Omar <oomar@umich.edu>
2023-09-09 14:21:34 -0700

    add japan to worldtime script

--- Linux/scripts/worldtime.sh
+++ Linux/scripts/worldtime.sh
@@ -3,8 +3,10 @@
 # Simple script which lists tracked times across the world.
 LEB_TIME=$(TZ=EET date)
 MI_TIME=$(TZ=":US/Eastern" date)
+JP_TIME=$(TZ=":Asia/Tokyo" date)
 LOCAL_TIME=$(date)
 
 echo -e "\033[1mLocal Time:   \033[0m $LOCAL_TIME"
 echo -e "\033[1mLebanon Time: \033[0m $LEB_TIME"
 echo -e "\033[1mMichigan Time:\033[0m $MI_TIME"
+echo -e "\033[1mJapan Time:   \033[0m $JP_TIME"
