653339d32e0ba7ee3c4e3d5fb66970ab1eabd01e
Omar Omar <oomar@umich.edu>
2024-02-02 00:00:59 -0800

    Update switching windows with arrow keys

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -18,10 +18,10 @@ M.general = {
   n = {
     ["<Esc>"] = { "<cmd> noh <CR>", "Clear highlights" },
     -- switch between windows
-    ["<C-h>"] = { "<C-w>h", "Window left" },
-    ["<C-l>"] = { "<C-w>l", "Window right" },
-    ["<C-j>"] = { "<C-w>j", "Window down" },
-    ["<C-k>"] = { "<C-w>k", "Window up" },
+    ["<C-Left>"]  = { "<C-w>h", "Window left" },
+    ["<C-Right>"] = { "<C-w>l", "Window right" },
+    ["<C-Down>"]  = { "<C-w>j", "Window down" },
+    ["<C-Up>"]    = { "<C-w>k", "Window up" },
 
     -- save
     ["<C-s>"] = { "<cmd> w <CR>", "Save file" },
